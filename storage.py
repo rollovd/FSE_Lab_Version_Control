ad534a1e87991b60ffccfdebbdd9b55bd3e00c22
Svetlana Gabdullina <ya.koroleva98@yandex.ru>
2020-10-04 01:59:58 +0300

    A typo was fixed

--- storage.py
+++ storage.py
@@ -20,6 +20,6 @@ class Storage:
     
     def add(self, key, value):
         if key in self.data:
-            raise Exeption
+            raise Exception
         else:
             self.data[key] = value
